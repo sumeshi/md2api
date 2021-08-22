fd42dd2da05c1af677cc165699491d77ae50f418
sumeshi <j15322sn@gmail.com>
2021-08-22 20:02:42 +0900

    feat: convert example

--- src/md2api/run.py
+++ src/md2api/run.py
@@ -1,8 +1,27 @@
 # coding: utf-8
+import sys
+from pathlib import Path
+from typing import List
+
+from markdown import Markdown
+
+
+def parse_markdown_filepaths(path: str) -> List[Path]:
+    root = Path(path)
+    return root.glob('**/*.md') if root.is_dir() else [root]
+
+def convert_markdown_to_html(text: str) -> str:
+    md = Markdown()
+    return md.convert(text)
+
 
 def main():
-    pass
+    target_path = Path(sys.argv[1])
+    markdown_files = parse_markdown_filepaths(target_path)
 
+    for f in markdown_files:
+        print(f)
+        print(convert_markdown_to_html(f.read_text()))
 
 if __name__ == '__main__':
     main()
