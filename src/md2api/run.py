008ede630b122cf1ab78c67499a4b1b28a350fd6
sumeshi <j15322sn@gmail.com>
2021-08-23 00:02:57 +0900

    feat: add func of get latest commit

--- src/md2api/run.py
+++ src/md2api/run.py
@@ -2,7 +2,9 @@
 import sys
 from pathlib import Path
 from typing import List
+from datetime import datetime
 
+from git import Repo
 from markdown import Markdown
 
 
@@ -10,10 +12,17 @@ def parse_markdown_filepaths(path: str) -> List[Path]:
     root = Path(path)
     return root.glob('**/*.md') if root.is_dir() else [root]
 
+
 def convert_markdown_to_html(text: str) -> str:
     md = Markdown()
     return md.convert(text)
 
+
+def get_lastcommit_date(path: str) -> datetime:
+    repo = Repo('.')
+    latest_commit = repo.iter_commits('--all', max_count=1, paths=path).__next__()
+    latest_committed_date = latest_commit.committed_date
+    return datetime.fromtimestamp(latest_committed_date)
 
 def main():
     target_path = Path(sys.argv[1])
@@ -22,6 +31,8 @@ def main():
     for f in markdown_files:
         print(f)
         print(convert_markdown_to_html(f.read_text()))
+        print(get_lastcommit_date(f))
+        print()
 
 if __name__ == '__main__':
     main()
